09c926dd61acd41bf37b610a98685ca45c267fd8
matthew.drees@uscm.org <matthew.drees@uscm.org@6185d61c-cf3b-044e-8ca2-f4aa9eb47c87>
2011-03-17 17:42:11 +0000

    oops. This isn't supposed to be an automated test. This hung bamboo.
    git-svn-id: http://arkham.ccci.org/svn/java/trunk/commons/deployments@8202 6185d61c-cf3b-044e-8ca2-f4aa9eb47c87

--- src/test/java/org/ccci/windows/management/RemoteServiceControlInteractiveTest.java
+++ src/test/java/org/ccci/windows/management/RemoteServiceControlInteractiveTest.java
@@ -13,7 +13,6 @@ public class RemoteServiceControlInteractiveTest
 
     RemoteServiceControl remoteServiceControl;
     
-    @Test
     public void restartService() throws JIException, IOException, InterruptedException
     {
         String password = ConsoleUtil.readPasswordFromInput();
