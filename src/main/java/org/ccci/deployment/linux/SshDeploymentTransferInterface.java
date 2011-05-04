602d3418be3d4e98da69b834e97bf8e89508d5cf
matthew.drees@uscm.org <matthew.drees@uscm.org@6185d61c-cf3b-044e-8ca2-f4aa9eb47c87>
2011-05-04 04:26:29 +0000

    oops.
    git-svn-id: http://arkham.ccci.org/svn/java/trunk/commons/deployments@8412 6185d61c-cf3b-044e-8ca2-f4aa9eb47c87

--- src/main/java/org/ccci/deployment/linux/SshDeploymentTransferInterface.java
+++ src/main/java/org/ccci/deployment/linux/SshDeploymentTransferInterface.java
@@ -139,7 +139,7 @@ public class SshDeploymentTransferInterface implements DeploymentTransferInterfa
         String localFilePath = localStorage.getDeploymentLocation().getPath() + "/" + deployment.getName() + ".war";
         try
         {
-            session.sendFile(localFilePath, remoteDeploymentDirectory, warFileName + ".tmp", "0644");
+            session.sendFile(localFilePath, remoteTransferDirectory, warFileName + ".tmp", "0644");
         }
         catch (IOException e)
         {
