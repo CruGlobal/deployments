1cf70be046ccf724f0628affb95e75ca32f89cc9
matthew.drees@uscm.org <matthew.drees@uscm.org@6185d61c-cf3b-044e-8ca2-f4aa9eb47c87>
2012-02-03 18:23:01 +0000

    initial work towards making the password able to be retrieved from an env variable; we need this on Bamboo
    git-svn-id: http://arkham.ccci.org/svn/java/trunk/commons/deployments@10459 6185d61c-cf3b-044e-8ca2-f4aa9eb47c87

--- deployments-maven-plugin/src/main/java/org/ccci/deployment/maven/DeploymentsMojo.java
+++ deployments-maven-plugin/src/main/java/org/ccci/deployment/maven/DeploymentsMojo.java
@@ -4,6 +4,7 @@ package org.ccci.deployment.maven;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.ccci.deployment.DeploymentDriver;
+import org.jboss.seam.util.Strings;
 
 /**
  * Goal which deploys the configured application
@@ -14,11 +15,48 @@ import org.ccci.deployment.DeploymentDriver;
 public class DeploymentsMojo
     extends AbstractMojo
 {
+    /**
+     * The name of the environment variable that contains the password to use for deployment.
+     * 
+     * @parameter expression="${deployments.passwordEnvironmentVariableName}" 
+     */
+    private String passwordEnvironmentVariableName;
 
     public void execute()
         throws MojoExecutionException
     {
         
-        DeploymentDriver driver = null;
+        
+        String password;
+        if (Strings.isEmpty(passwordEnvironmentVariableName))
+        {
+            getLog().warn("did not provide passwordEnvironmentVariableName");
+            return;
+        }
+        else
+        {
+            password = System.getenv(passwordEnvironmentVariableName);
+        }
+        
+        if (Strings.isEmpty(password))
+        {
+            getLog().warn("Unable to get password from $" + passwordEnvironmentVariableName);
+        }
+        else
+        {
+            getLog().info("got password; it is " + password.length() + " characters long");
+        }
     }
+
+    public String getPasswordEnvironmentVariableName()
+    {
+        return passwordEnvironmentVariableName;
+    }
+
+    public void setPasswordEnvironmentVariableName(String passwordEnvironmentVariableName)
+    {
+        this.passwordEnvironmentVariableName = passwordEnvironmentVariableName;
+    }
+    
+    
 }
