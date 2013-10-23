4ec60003754afbf3ae372ec42647f29adabc81aa
Matthew Drees <matt.drees@cru.org>
2013-10-23 13:32:36 -0600

    preserve backwards compatibility
    (for now)

---
+++ deployments-core/src/main/java/org/ccci/deployment/BasicWebappDeployment.java
@@ -0,0 +1,9 @@
+package org.ccci.deployment;
+
+/**
+ * @author Matt Drees
+ *
+ * Only for now, and only for backwards compatibilitly
+ */
+public class BasicWebappDeployment extends org.ccci.deployment.basic.BasicWebappDeployment{
+}
