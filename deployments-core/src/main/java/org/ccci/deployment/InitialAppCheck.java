25e8f61b84043118c298d6afa360f5dfa4acfff0
Matthew Drees <matt.drees@cru.org>
2013-11-12 17:32:47 -0700

    finesse the initial app check
    * don't abort if there's only one node and it's down
    * don't abort if node 1 is the only one down

--- deployments-core/src/main/java/org/ccci/deployment/InitialAppCheck.java
+++ deployments-core/src/main/java/org/ccci/deployment/InitialAppCheck.java
@@ -4,6 +4,8 @@ import org.apache.log4j.Logger;
 import org.ccci.deployment.spi.DeploymentConfiguration;
 import org.ccci.deployment.spi.WebappControlInterface;
 
+import java.util.List;
+
 
 /**
  * @author Matt Drees
@@ -18,13 +20,47 @@ public class InitialAppCheck {
         this.configuration = configuration;
     }
 
+    /**
+     * Verifies we don't deploy to the first node when the second node is dead.
+     * (unless the first node is also dead, in which case we're already experiencing downtime)
+     */
     public void check() {
-        for (Node node : configuration.listNodes())
-        {
-            log.info("checking app is up on " + node.getName());
+        List<Node> nodes = configuration.listNodes();
 
+        if (nodes.size() < 2)
+            return;
+
+        boolean first = true;
+        Exception nodeProblem = null;
+        for (Node node : nodes) {
+            log.info("checking app is up on " + node.getName());
+            nodeProblem = checkNode(node);
+            if (first) {
+                if (nodeProblem != null) {
+                    log.warn(node + " is down. Hopefully the deployment will resolve this.", nodeProblem);
+                    return;
+                }
+                first = false;
+            } else {
+                if (nodeProblem == null)
+                    return;
+            }
+        }
+
+        throw new RuntimeException(
+            "The first node is up, but the rest are down!  Better not deploy right now.",
+            nodeProblem);
+    }
+
+    //TODO: this could be better.  No need to wait 2m or whatever if the node is down.
+    private Exception checkNode(Node node) {
+        try {
             WebappControlInterface webappControlInterface = configuration.buildWebappControlInterface(node);
             webappControlInterface.verifyNewDeploymentActive();
+            return null;
+        } catch (Exception e) {
+            return e;
         }
+
     }
 }
